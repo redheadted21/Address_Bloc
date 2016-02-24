fa916550a71d965572994b2c75fae4fb00bc7795
Teddy <teddyhealysmith@gmail.com>
2016-02-23 16:30:38 -0800

    Add Checkpoint 19

---
+++ models/address_book.rb
@@ -0,0 +1,37 @@
+require_relative 'entry'
+
+class AddressBook
+  attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+
+    index = 0
+    @entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if (entry.name == name && entry.phone_number == phone_number && entry.email == email)
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)
+  end
+
+
+
+end
